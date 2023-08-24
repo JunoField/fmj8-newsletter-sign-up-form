c1bcdf4dcf94c7a546bd260831abcdbfac3b6e1a
JunoField <junofield123@gmail.com>
2023-08-24 16:54:25 +0100

    done mobile success layout

--- src/SuccessView.jsx
+++ src/SuccessView.jsx
@@ -7,7 +7,10 @@ function SuccessView() {
         <img src="../assets/images/icon-success.svg" />
         <h1>Thanks for subscribing!</h1>
         <p>A confirmation email has been sent to <strong>ash@loremcompany.com</strong>. Please open it and click the button inside to confirm your subscription.</p>
-
+        
+        <div className="successFooter">
+        <button className="input btn">Dismiss message</button>
+        </div>
       </div>
   )
 }
