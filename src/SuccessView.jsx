3610b165873ddcb727461e3ce248cb0aaa49033e
JunoField <junofield123@gmail.com>
2023-08-24 16:39:38 +0100

    started on success layout

--- src/SuccessView.jsx
+++ src/SuccessView.jsx
@@ -3,8 +3,10 @@ import './App.css'
 function SuccessView() {
 
   return (
-      <div className="cardRoot">
-        Congrats
+      <div className="cardRoot successCardRoot">
+        <img src="../assets/images/icon-success.svg" />
+        <h1>Thanks for subscribing!</h1>
+        <p>A confirmation email has been sent to <strong>ash@loremcompany.com</strong>. Please open it and click the button inside to confirm your subscription.</p>
       </div>
   )
 }
