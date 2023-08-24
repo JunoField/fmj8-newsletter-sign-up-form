cfd84c1de117332dde2bd37a45f61f4670342226
JunoField <junofield123@gmail.com>
2023-08-24 20:41:34 +0100

    started on validation, have a weird error where it submits regardless of validity

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -4,17 +4,33 @@ import { useState } from "react";
 function SignUpForm({setEmail}) {
 
     const [enteredEmail, setEnteredEmail] = useState("");
+    const [validity, setValidity] = useState(true);
+
+    const validateEmail = (input) => input.toLowerCase()
+        .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Submitted");
-        setEmail(enteredEmail);
+        setValidity(validateEmail(enteredEmail));
+        if (validity){
+            setEmail(enteredEmail);
+        }
     };
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setEnteredEmail(e.target.value);
+        setValidity(validateEmail(enteredEmail));
+    }
 
   return (
       <form onSubmit={handleSubmit}>
-        <label>Email address</label>
-        <input type="email" placeholder="email@company.com" className="input" value={enteredEmail} onChange={e => setEnteredEmail(e.target.value)}/>
+        <div className="inputHeader">
+            <label>Email address</label>
+            {!validity && <label className="errorMessage">Valid email required</label>}
+        </div>
+        <input type="email" placeholder="email@company.com" className={`input ${!validity && "inputError"}`} value={enteredEmail} onChange={handleChange} />
         <input type="submit" value="Subscribe to monthly newsletter" className="input btn" />
       </form>
   )
