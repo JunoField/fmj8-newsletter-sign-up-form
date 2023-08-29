c89ac854d4ede108b6a7f0b6d76ecf86364a6e48
JunoField <junofield123@gmail.com>
2023-08-29 21:37:51 +0100

    added slash to base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/fmj8-newsletter-sign-up-form",
+    base: "/fmj8-newsletter-sign-up-form/",
   plugins: [react()],
 })
