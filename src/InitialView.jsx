cfc8d45d83d70381882047759b88b3bb4801b877
JunoField <junofield123@gmail.com>
2023-08-29 17:33:25 +0100

    done 'mid width' layout between desktop and mobile

--- src/InitialView.jsx
+++ src/InitialView.jsx
@@ -4,8 +4,8 @@ import InitialTextBody from "./InitialTextBody.jsx";
 function InitialView({setEmail}) {
 
   return (
-      <div className="cardRoot">
-        <img src="../assets/images/illustration-sign-up-mobile.svg" />
+      <div className="cardRoot initialCardRoot">
+        <img src="../assets/images/illustration-sign-up-mobile.svg" className="imgMobile"/>
 
         <InitialTextBody setEmail={setEmail}/>
       </div>
